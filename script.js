9d45b30cdda913f60b00046b0a581b35cba662f7
DenilsonCunha <denilsonluciano6@gmail.com>
2021-05-04 10:34:27 -0300

    1JS

---
+++ script.js
@@ -0,0 +1,18 @@
+function tabuada() {
+    let valor = document.getElementById('num')
+   let tab = document.getElementById('seltab')
+  if (valor.value.length == 0) {
+        alert('Por favor, Digite um número!')
+} else {
+    var n = Number(valor.value)
+    var c = 1
+    tab.innerHTML = ''
+    while (c -= 10) {
+      var item = document.createElement('option')
+      item.text = `${n} * ${c} = ${c*n}`
+      item.value = `tab${c}`
+      tab.appendChild(item)
+      c++
+    }
+  }
+}
